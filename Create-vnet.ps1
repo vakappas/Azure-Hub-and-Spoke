1b379d5ef1128441f129aee3b464756656e93ce4
Vaggelis Kappas <vakappas@microsoft.com>
2019-07-16 22:16:37 +0300

    Change default nvaType

--- Create-vnet.ps1
+++ Create-vnet.ps1
@@ -11,7 +11,7 @@ $Location = "West Europe"
 ## $url="https://raw.githubusercontent.com/vakappas/Azure-Hub-and-Spoke/Hub-net-2-firewalls-1-router/hub-vnet.json"
 
 ## url=https://raw.githubusercontent.com/vakappas/Azure-Hub-and-Spoke/master/spoke-vnet.json
-$url="https://raw.githubusercontent.com/vakappas/Azure-Hub-and-Spoke/Hub-Spoke-On-Prem/Hub-Spoke-On-Prem-master.json"
+$url="https://raw.githubusercontent.com/vakappas/Azure-Hub-and-Spoke/Hub-Spoke-On-Prem/on-prem-vnet.json"
 
 New-AzResourceGroup -Name $RG -Location $Location
 
@@ -19,7 +19,8 @@ $cred = Get-Credential
 $templateParameters = @{
 
     adminUsername = $cred.UserName
-    adminPassword = $cred.Password   
+    adminPassword = $cred.Password
+    domainName = "vklab.local"
 
 }
 
