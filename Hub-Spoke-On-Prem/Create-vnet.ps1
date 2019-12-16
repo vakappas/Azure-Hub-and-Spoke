3247a30311a731a978fdd466a8c608efff6e0490
Vaggelis Kappas <vakappas@microsoft.com>
2019-12-16 12:33:12 +0200

    Turn the VPN creation to Off

--- Hub-Spoke-On-Prem/Create-vnet.ps1
+++ Hub-Spoke-On-Prem/Create-vnet.ps1
@@ -11,7 +11,7 @@ $Location = "West Europe"
 ## $url="https://raw.githubusercontent.com/vakappas/Azure-Hub-and-Spoke/Hub-net-2-firewalls-1-router/hub-vnet.json"
 
 ## url=https://raw.githubusercontent.com/vakappas/Azure-Hub-and-Spoke/master/spoke-vnet.json
-$url="https://raw.githubusercontent.com/vakappas/Azure-Hub-and-Spoke/Hub-Spoke-On-Prem/Hub-Spoke-On-Prem-master.json"
+$url="https://raw.githubusercontent.com/vakappas/Azure-Hub-and-Spoke/master/Hub-Spoke-On-Prem/Hub-Spoke-On-Prem-master.json"
 
 New-AzResourceGroup -Name $RG -Location $Location
 
